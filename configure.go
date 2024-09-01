ff905d2eab0bf1deead44a12bcc51cdddac6f782
Rafael Godoy <contact@rafaelgodoy.com>
2024-09-01 13:57:52 -0300

    Crawler from boot dot dev

---
+++ configure.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"sync"
+)
+
+type config struct {
+	pages              map[string]int
+	baseURL            *url.URL
+	mut                *sync.Mutex
+	concurrencyControl chan struct{}
+	wg                 *sync.WaitGroup
+	maxPages           int
+}
+
+func configure(rawBaseURL string, maxConcurrency int, maxPages int) (*config, error) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
+	}
+
+	return &config{
+		pages:              make(map[string]int),
+		baseURL:            baseURL,
+		mut:                &sync.Mutex{},
+		concurrencyControl: make(chan struct{}, maxConcurrency),
+		wg:                 &sync.WaitGroup{},
+		maxPages:           maxPages,
+	}, nil
+}
+
+func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
+	cfg.mut.Lock()
+	defer cfg.mut.Unlock()
+
+	if _, visited := cfg.pages[normalizedURL]; visited {
+		cfg.pages[normalizedURL]++
+		return false
+	}
+
+	cfg.pages[normalizedURL] = 1
+	return true
+}
+
+func (cfg *config) pagesLen() int {
+	cfg.mut.Lock()
+	defer cfg.mut.Unlock()
+	return len(cfg.pages)
+}
